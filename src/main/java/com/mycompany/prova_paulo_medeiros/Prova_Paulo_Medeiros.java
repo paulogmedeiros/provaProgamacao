fd568703ff40a6b6c08fdeb14916ceb63d14cb5a
paulogmedeiros <paulogsantosm@gmail.com>
2023-10-16 19:06:50 -0300

    teste

---
+++ src/main/java/com/mycompany/prova_paulo_medeiros/Prova_Paulo_Medeiros.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prova_paulo_medeiros;
+
+/**
+ *
+ * @author pmedeiros
+ */
+public class Prova_Paulo_Medeiros {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
